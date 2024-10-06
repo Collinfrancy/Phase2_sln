c405fd7e408081e88916d2b0e7a82570ff10d9e1
Collin Francy <116471845+Collinfrancy@users.noreply.github.com>
2024-10-06 08:19:46 +0530

    Add files via upload

---
+++ PersonProject/PersonConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonProject
+{
+    static class PersonConfig
+    {
+        public static EditPersonWindow FrmEditPersom {  get; set; }
+        public static PersonViewModel VueModel { get; set; }
+
+
+        static PersonConfig()
+        {
+            VueModel=new PersonViewModel();
+            FrmEditPersom = new EditPersonWindow();
+    }
+
+
+    }
+
+    
+
+}
---
+++ PersonProject/PersonViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace PersonProject
+{
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private PersonModel _displayPerson;
+        public PersonModel DisplayPerson {  get { return _displayPerson; }
+            set { _displayPerson = value; OnPropertyChanged(nameof(DisplayPerson)); }
+        }
+
+
+        private PersonModel _editablePerson;
+        public PersonModel EditablePerson
+        {
+            get { return _editablePerson; }
+            set { _editablePerson = value; OnPropertyChanged(nameof(EditablePerson)); }
+        }
+
+
+        public ICommand UpdateCommand {  get; }
+
+        public PersonViewModel() {
+            DisplayPerson = new PersonModel { FirstName = "Collin", LastName = "F" };
+            EditablePerson = new PersonModel {FirstName="",LastName=""};
+            UpdateCommand = new RelayCommand(Update);
+        
+        }
+
+
+        public void Update() 
+        {
+
+            var result = MessageBox.Show(messageBoxText: "Are you sure to update",
+                      caption: "Confirm",
+                      button: MessageBoxButton.YesNo,
+                      icon: MessageBoxImage.Question);
+            if (result != MessageBoxResult.Yes) { return; }
+            DisplayPerson.FirstName = EditablePerson.FirstName;
+            DisplayPerson.LastName = EditablePerson.LastName;
+            DisplayPerson = DisplayPerson;
+             result = MessageBox.Show(messageBoxText: "Updated Successfully",
+                  caption: "Alert",
+                  button: MessageBoxButton.OK,
+                  icon: MessageBoxImage.Information);
+            if (EditClose != null) { EditClose(); }
+
+
+        }
+        public DClose EditClose;
+
+        public void Edit()
+        {
+            EditablePerson.FirstName = DisplayPerson.FirstName;
+            EditablePerson.LastName = DisplayPerson.LastName;
+            EditablePerson = EditablePerson;
+        }
+
+    }
+}
