34c954d2b7eaff6cb54314baedccfb47ffef0a77
Collin Francy <116471845+Collinfrancy@users.noreply.github.com>
2024-10-06 08:23:21 +0530

    Add files via upload

---
+++ Example13Project/MenuWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Example13Project
+{
+    /// <summary>
+    /// Interaction logic for MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+        private MainWindow frmmainWindow=new MainWindow();
+        private SquareCalc frmSquareCalc =new SquareCalc();
+        private sumCalc frmSumCalc = new sumCalc();
+
+        private void btnCounter_Click(object sender, RoutedEventArgs e)
+        {
+            
+            frmmainWindow.Show();
+            frmmainWindow.Activate();
+        }
+
+        private void btnSumCalc_Click(object sender, RoutedEventArgs e)
+        {
+
+            frmSumCalc.Show();
+            frmSumCalc.Activate();
+        }
+
+        private void btnSquare_Click(object sender, RoutedEventArgs e)
+        {
+
+            frmSquareCalc.Show();
+            frmSquareCalc.Activate();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+    }
+}
---
+++ Example13Project/SquareCalc.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Example13Project
+{
+    /// <summary>
+    /// Interaction logic for SquareCalc.xaml
+    /// </summary>
+    public partial class SquareCalc : Window
+    {
+        public SquareCalc()
+        {
+            InitializeComponent();
+        }
+        private bool IsNum(string numStr)
+        {
+            try
+            {
+                int.Parse(numStr);
+                return true;
+            }
+            catch { return false; }
+        }
+
+        private void btnSquare_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsNum(txtNumber.Text))
+            {
+                MessageBox.Show("Please enter Number");
+                txtNumber.Focus();
+                txtNumber.SelectAll();
+                return;
+            }
+            int number = int.Parse(txtNumber.Text);
+        
+            int result = number*number;
+            lblResult.Text = "Square";
+            txtResult.Text = result.ToString();
+
+        }
+
+        private void btnCube_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsNum(txtNumber.Text))
+            {
+                MessageBox.Show("Please enter Number");
+                txtNumber.Focus();
+                txtNumber.SelectAll();
+                return;
+            }
+            int number = int.Parse(txtNumber.Text);
+            lblResult.Text = "cube";
+
+            int result = number * number * number;
+            txtResult.Text = result.ToString();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+    }
+}
