a8230cf226e0fcc6a3681c573ddb41fe56aa5797
Collin Francy <116471845+Collinfrancy@users.noreply.github.com>
2024-10-07 07:29:15 +0530

    Add files via upload

---
+++ BankManagementSystem_sln/BankManagementSystem/Databases/AccountMemoryRepo.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using BankManagementSystem.Exceptions;
+using BankManagementSystem.Models;
+using BankManagementSystem.Repos;
+
+namespace BankManagementSystem.Databases
+{
+    public class AccountMemoryRepo : IAccountRepo
+    {
+        private static AccountMemoryRepo _instance;
+        private ObservableCollection<Account> accounts;
+
+        private AccountMemoryRepo()
+        {
+            accounts = new ObservableCollection<Account>();
+            InitializeAccounts();
+        
+        }
+
+        private void InitializeAccounts()
+        {
+            accounts.Add(new Account
+            {
+                AccountNumber = 1234,
+                Name = "febin",
+                Balance = 0,
+                Type = "savings",
+                Email = "febin@gmail.com",
+                PhoneNumber = "5236526526",
+                Address = "gdsagdhsgdhsg",
+                IsActive = true,
+                InterestPercentage = "0",
+                TransactionCount = 0,
+                LastTransactionDate = DateTime.Now,
+            });
+            accounts.Add(new Account
+            {
+                AccountNumber = 3132324,
+                Name = "abhinav",
+                Balance = 0,
+                Type = "current",
+                Email = "abhinav@gmail.com",
+                PhoneNumber = "5236526526",
+                Address = "gdsagdhsgdhsg",
+                IsActive = true,
+                InterestPercentage = "0",
+                TransactionCount = 0,
+                LastTransactionDate = DateTime.Now,
+            });
+        }
+
+        public static AccountMemoryRepo Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new AccountMemoryRepo();
+                }
+                return _instance;
+            }
+        }
+
+
+        public void Create(Account account)
+        {
+            try
+            {
+                accounts.Add(account);
+            }
+            catch(AccountException ae)
+            {
+                throw new AccountException("Error in creating account");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            
+        }
+
+        public void UpdateAccount(Account account)
+        {
+            try
+            {
+                var existingAccount = accounts.FirstOrDefault(a => a.AccountNumber == account.AccountNumber);
+                if (existingAccount != null)
+                {
+                    existingAccount.Address = account.Address;
+                }
+                else
+                {
+                    throw new AccountException("Account doesn't exists");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        public ObservableCollection<Account> ReadAllAccount()
+        {
+            try
+            {
+                return accounts;
+            }
+            catch(AccountException ae)
+            {
+                throw new AccountException("Error reading accounts");
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            
+        }
+
+        public void DeleteAccount(int acNo, Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deposit(int acNo, int Amount)
+        {
+            try
+            {
+                var account = accounts.FirstOrDefault(a => a.AccountNumber == acNo);
+                if (account != null)
+                {
+                    account.Balance = account.Balance + Amount;
+                    account.LastTransactionDate = DateTime.Now;
+                    account.TransactionCount = account.TransactionCount + 1;
+
+                }
+                else
+                {
+                    throw new AccountException("Account Not Found , Please input valid account number");
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Withdrw(int acNo, int Amount)
+        {
+            try
+            {
+                var account = accounts.FirstOrDefault(a => a.AccountNumber == acNo);
+                if (account != null)
+                {
+                    if (account.Balance < Amount)
+                    {
+                        throw new AccountException("Insufficient balance");
+                        
+                    }
+                    account.Balance = account.Balance - Amount;
+                    account.LastTransactionDate = DateTime.Now;
+                    account.TransactionCount = account.TransactionCount + 1;               
+
+                }
+                else
+                {
+                    throw new AccountException("Account Not Found , Please input valid account number");
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            
+        }
+
+        public void CalculateInterestAndUpdateBalance()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BankManagementSystem_sln/BankManagementSystem/ViewModels/DepositViewModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankManagementSystem.Databases;
+using BankManagementSystem.Models;
+using BankManagementSystem.Repos;
+using System.Windows.Input;
+using BankManagementSystem.Commands;
+using System.Windows;
+using BankManagementSystem.Exceptions;
+
+namespace BankManagementSystem.ViewModels
+{
+    public class DepositViewModel : ViewModelBase
+    {
+        private int _accountNumber;
+        public int AccountNumber
+        {
+            get { return _accountNumber; }
+            set 
+            {
+                _accountNumber = value; 
+                onPropertyChanged(nameof(AccountNumber));
+            }
+
+        }
+
+        private int _amount;
+
+        public int Amount
+        {
+            get { return _amount; }
+            set 
+            {
+                _amount = value;
+                onPropertyChanged(nameof(Amount));
+            }
+        }
+
+        private IAccountRepo _repo = AccountMemoryRepo.Instance;
+
+        public ICommand DepositCommand {  get;}
+        
+        public DepositViewModel()
+        {
+            DepositCommand = new RelayCommand(Deposit);
+        }
+
+        public void Deposit()
+        {
+            var result = MessageBox.Show(messageBoxText: "Are you sure to Deposit?",
+                   caption: "Confirm",
+                   button: MessageBoxButton.YesNo,
+                   icon: MessageBoxImage.Question);
+            if (result != MessageBoxResult.Yes)
+            {
+                return;
+            }
+            try
+            {
+                _repo.Deposit(AccountNumber, Amount);
+                MessageBox.Show(messageBoxText: $"Deposited Successfully to account {AccountNumber}",
+                        caption: "Alert",
+                        button: MessageBoxButton.OK,
+                        icon: MessageBoxImage.Information);
+                Logger.log.Info($"Deposited {Amount} rupees Successfully to account {AccountNumber}");
+                this.AccountNumber = 0;
+                this.Amount = 0;
+            }
+            catch(AccountException ae)
+            {
+                MessageBox.Show(messageBoxText: $"{ae.Message}",
+               caption: "Warning",
+               button: MessageBoxButton.OK,
+               icon: MessageBoxImage.Warning);
+
+                Logger.log.Error(ae.Message);
+            }
+            catch (Exception ex)
+            {
+                Logger.log.Error(ex.Message);
+            }
+
+
+
+            
+        }
+
+
+
+
+
+    }
+}
